517ee818ff23ba258cf28dee888078663ce42ebb
flavio b <flavio.b.github@gmail.com>
2023-10-01 16:31:27 +0100

    init app

---
+++ src/gateway/src/app/not_found.rs
@@ -0,0 +1,20 @@
+use actix_web::{HttpRequest, HttpResponse};
+use leptos::{view, IntoView};
+
+use crate::app::component::MyHtml;
+
+pub async fn not_found(_req: HttpRequest) -> HttpResponse {
+    let html = leptos::ssr::render_to_string(|| {
+        view! {
+            <MyHtml>
+                <div class="container text-light" >
+                    <h1>"Not Found Page!"</h1>
+                </div>
+            </MyHtml>
+        }
+    });
+
+    HttpResponse::NotFound()
+        .content_type("text/html; charset=utf-8")
+        .body(html.as_str().to_string())
+}
--- src/gateway/src/lib.rs
+++ src/gateway/src/lib.rs
@@ -1,4 +1,5 @@
 mod api;
+mod app;
 mod db;
 mod truelayer;
 
@@ -45,8 +46,12 @@ pub async fn start(config: AppConfig) -> anyhow::Result<()> {
             .wrap(RequestTracing::new())
             .wrap(TracingLogger::default())
             .service(web::resource("/health_check").route(web::get().to(HttpResponse::Ok)))
+            .service(web::resource("/").to(app::home))
+            .service(web::resource("/payment").to(app::payment))
+            .service(web::resource("/deposit").to(app::deposit))
             .service(api::create_payment::create_payment)
             .service(api::deposit_payment::deposit_payment)
+            .default_service(web::to(app::not_found))
     })
     .bind(("0.0.0.0", config.http_port))?
     .run();
