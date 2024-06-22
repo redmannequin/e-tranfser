cd4742765aa2c62525de0f4d86b73e5bc77ed410
flavio b <flavio.b.github@gmail.com>
2024-06-22 18:56:21 +0100

    rename depoist pages

--- src/gateway/src/app/deposit_flow/mod.rs
+++ src/gateway/src/app/deposit_flow/mod.rs
@@ -1,15 +1,15 @@
-mod create_payout;
-mod deposit_form;
-mod deposit_status;
-mod depsoit_select_account;
-mod tl_despoit_callback;
+mod z01_deposit_form;
+mod z03_tl_despoit_callback;
+mod z04_depsoit_select_account;
+mod z05_create_payout;
+mod z06_deposit_status;
 
 use actix_web::web;
-use create_payout::create_payout;
-use deposit_form::deposit_form;
-use deposit_status::{deposit_status, deposit_status_update};
-use depsoit_select_account::deposit_select_account;
-use tl_despoit_callback::tl_deposit_callback;
+use z01_deposit_form::deposit_form;
+use z03_tl_despoit_callback::tl_deposit_callback;
+use z04_depsoit_select_account::deposit_select_account;
+use z05_create_payout::create_payout;
+use z06_deposit_status::{deposit_status, deposit_status_update};
 
 pub const DESPOSIT_CREATE_PAGE: &str = "/app/deposit";
 pub const DESPOSIT_SELECT_ACCOUNT_PAGE: &str = "/app/deposit/select_account";
@@ -24,9 +24,9 @@ pub const PAYOUT_COOKIE: &str = "payout_init";
 pub fn deposit_scope() -> actix_web::Scope {
     web::scope("deposit")
         .service(web::resource("").to(deposit_form))
-        .service(web::resource("create_payout").get(create_payout))
+        .service(web::resource("tl_callback").to(tl_deposit_callback))
         .service(web::resource("select_account").to(deposit_select_account))
+        .service(web::resource("create_payout").get(create_payout))
         .service(web::resource("status").to(deposit_status))
         .service(web::resource("status_update").to(deposit_status_update))
-        .service(web::resource("tl_callback").to(tl_deposit_callback))
 }
