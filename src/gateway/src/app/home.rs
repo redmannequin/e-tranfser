aa8a5bb316752099a152cafbc5758fe0fad904ba
flavio b <flavio.b.github@gmail.com>
2024-07-04 19:56:47 +0100

    move money only

--- src/gateway/src/app/home.rs
+++ src/gateway/src/app/home.rs
@@ -1,9 +1,7 @@
 use actix_web::{HttpRequest, HttpResponse};
 use leptos::view;
 
-use crate::app::{
-    component::MyHtml, payment_flow::PAYMENT_FORM_PAGE, registration_flow::REGISTER_PAGE,
-};
+use crate::app::{component::MyHtml, payment_flow::PAYMENT_FORM_PAGE};
 
 pub async fn home(_req: HttpRequest) -> HttpResponse {
     let html = leptos::ssr::render_to_string(|| {
@@ -12,8 +10,8 @@ pub async fn home(_req: HttpRequest) -> HttpResponse {
                 <div class="container text-light text-center pt-4" >
                     <h1 class="text-center" >"Welcome to e-transfer"</h1>
                     <a class="btn btn-success" href={PAYMENT_FORM_PAGE} >Move Money</a>
-                    <a class="btn btn-success ms-1" href="/login" >Sign In</a>
-                    <a class="btn btn-success ms-1" href={REGISTER_PAGE} >Register</a>
+                    // <a class="btn btn-success ms-1" href="/login" >Sign In</a>
+                    // <a class="btn btn-success ms-1" href={REGISTER_PAGE} >Register</a>
                 </div>
             </MyHtml>
         }
